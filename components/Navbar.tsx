9e0635e853add0648bc737337af9fb6c9d572575
matheusrgg <matheusrgg@gmail.com>
2023-11-21 17:17:10 -0300

    bira

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,8 +39,13 @@ export default function Navbar() {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <div className="container flex items-center space-x-2">
               <Link to="home">
-                <h2 className="text-2xl font-bold">Matheus Ruegg</h2>
+                <div className="container flex items-center space-x-2">
+                  <div>
+                    <h2 className="text-2xl font-bold">Matheus Ruegg</h2>
+                  </div>
+                </div>
               </Link>
+
             </div>
             <div className="md:hidden">
               <button
